cf2780f90eea9520cdd8d902891dbe293649fda8
geryy <g@geryy.io>
2021-02-04 14:54:03 +0100

    add live preview & automatic file download

--- api/vaster/index.ts
+++ api/vaster/index.ts
@@ -1,6 +1,9 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 import xml2js from 'xml2js';
+import https from 'https';
+import url, { URL } from 'url';
+// import http from 'http'
 const parser = new xml2js.Parser({
 	// trim: true,
 	mergeAttrs: true,
@@ -11,31 +14,42 @@ const parser = new xml2js.Parser({
 module.exports = async (req: NowRequest, res: NowResponse) => {
 	const target = req.query.target as string;
 
-	if (!target) {
-		return res.send('missing target &target');
-	}
+	if (!target) throw new Error('missing target parameter &target');
 
-	console.log(target);
-
-	// // fetch XML
 	try {
-		const scrape = await fetch(target);
-		const xmlData = await scrape.text();
+		// 1 ) FETCH XML and scrape content
+		const scrapeResponse = await fetch(target);
+		const xmlData = await scrapeResponse.text();
 
-		if (xmlData) {
-			const data = await parser.parseStringPromise(xmlData);
-			const mediaFile = data.vast.ad[0].inline[0].creatives[0].creative[0].linear[0].mediafiles[0].mediafile[0];
+		if (!xmlData) throw new Error('no_valid_xml');
 
-			if (mediaFile) {
-				const fileURL = mediaFile._;
-				res.redirect(303, fileURL);
-			} else {
-				res.status(404).send('no media file url was found');
-			}
-		} else {
-			res.status(404).send('noting found in that xml');
-		}
+		// parse XML and LOOK for Media FIle
+		const data = await parser.parseStringPromise(xmlData);
+		const mediaFile = data.vast.ad[0].inline[0].creatives[0].creative[0].linear[0].mediafiles[0].mediafile[0];
+
+		if (!mediaFile) throw new Error('no_valid_media_file_in_xml');
+
+		const fileURL = mediaFile._; // media file link
+		const fileName = new URL(fileURL).pathname.split('/').pop();
+
+		// Download file on serverless function and forward file to client
+		https
+			.get(fileURL, (externalVastResponse) => {
+				// check if externalVastResponse was ok
+				if (externalVastResponse.statusCode !== 200) {
+					throw new Error('media_download');
+				}
+
+				res.setHeader('content-disposition', 'attachment; filename=' + fileName);
+				return externalVastResponse.pipe(res);
+			})
+			.on('error', (e) => {
+				console.log('Media Request faild: ' + e);
+				throw new Error('media_request');
+			});
 	} catch (error) {
-		res.status(500).json(JSON.stringify(error));
+		// todo - add sentry
+		console.error('🤐 ' + error);
+		res.status(500).json({ error: error.message || 'upsi' });
 	}
 };
